ff565bc45cc18e92a74c94a4293d17c359c9b112
devanshuappnwebtechnologies <devanshusharma@appnwebtechnologies.co.in>
2026-04-01 12:08:38 +0530

    Initial Commit

---
+++ Stylish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stylish
+//
+//  Created by Rakesh Gupta on 01/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stylish/StylishApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylishApp.swift
+//  Stylish
+//
+//  Created by Rakesh Gupta on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct StylishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
